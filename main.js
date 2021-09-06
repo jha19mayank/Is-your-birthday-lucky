e887e9bae5a93669ec13038cba874dd0f1139d65
jha19mayank <60752983+jha19mayank@users.noreply.github.com>
2021-09-06 17:30:08 +0530

    📝 updated output box message

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 const janamDin = document.querySelector("#date-of-birth");
 const luckyNumber = document.querySelector("#lucky-number");
 const checkButton = document.querySelector("#check-number");
+const outputBox = document.querySelector("#output-box");
 
 checkButton.addEventListener("click", checkBirthdayIsLucky);
 
 function checkBirthdayIsLucky() {
   const dob = janamDin.value;
   const sum = calculateSum(dob);
-  checkingYourLuck(sum, luckyNumber.value);
+  if (dob && sum) checkingYourLuck(sum, luckyNumber.value);
+  else {
+    outputBox.innerHTML = "Please enter both the input fields😤";
+  }
 }
 
 function calculateSum(dob) {
-  dob = dob.replaceAll("-", "0");
+  dob = dob.replaceAll("-", "");
   let sum = 0;
   for (let i = 0; i < dob.length; i++) {
     sum = sum + Number(dob.charAt(i));
@@ -21,8 +25,8 @@ function calculateSum(dob) {
 
 function checkingYourLuck(sum, luckyNumber) {
   if (sum % luckyNumber === 0) {
-    console.log("Your birthday is so lucky🎉");
+    outputBox.innerHTML = "Your birthday is so lucky🎉";
   } else {
-    console.log("Your birthday is not so lucky😐");
+    outputBox.innerHTML = "Your birthday is not so lucky😐";
   }
 }
